a2c440a4fade0fdd42ed898d119f65213998a172
Fergie77 <48885644+Fergie77@users.noreply.github.com>
2025-02-17 12:14:33 +0000

    V2 Animation

--- src/Animations/CollageAnimation.js
+++ src/Animations/CollageAnimation.js
@@ -1,8 +1,13 @@
 import gsap from 'gsap'
+import CustomEase from 'gsap/CustomEase'
 
 export const CollageAnimation = (container) => {
   const collage = container.querySelector('.collage-animation_layout')
-
+  gsap.registerPlugin(CustomEase)
+  const hardExpo = CustomEase.create(
+    'custom',
+    'M0,0 C0.01,0.074 0.03,0.414 0.054,0.502 0.082,0.604 0.154,0.742 0.2,0.8 0.252,0.865 0.374,0.981 1,0.981 '
+  )
   if (!collage) return
 
   gsap.set(collage.parentElement, {
@@ -11,49 +16,46 @@ export const CollageAnimation = (container) => {
 
   const mainTl = gsap.timeline({
     ease: 'none',
+    delay: 1,
     onComplete: () => {
-      masterTl.play(0)
+      // masterTl.play(0)
       mainTl.play(0)
     },
   })
 
   mainTl.to(collage, {
-    rotateY: 180,
-    duration: 3,
-    ease: 'expo.inOut',
+    rotateY: 200,
+    duration: 4,
+    ease: hardExpo,
   })
 
   mainTl.to(
     collage,
     {
-      rotateY: 200,
-      z: 200,
-      duration: 2,
-      ease: 'ease',
+      z: 100,
+      duration: 1,
+      ease: 'expo.inOut',
     },
-    '<2'
+    '<2.5'
   )
 
-  mainTl.to(
-    collage,
-    {
-      rotateY: 360,
-      duration: 3,
-      ease: 'expo.out',
-    },
-    '<1'
-  )
+  mainTl.to(collage, {
+    rotateY: 360,
+    z: 0,
+    duration: 3,
+    ease: hardExpo,
+  })
 
   const offsetX = [
-    -200, //-4
+    -300, //-4
     -120, //-3
     -150, //-2
     -90, //-1
     0, //0
     60, //1
-    100, //2
+    200, //2
     80, //3
-    150, //4
+    300, //4
   ]
   const offsetY = [
     -100, //-4
@@ -61,51 +63,58 @@ export const CollageAnimation = (container) => {
     150, //-2
     70, //-1
     0, //0
-    100, //1
+    200, //1
     120, //2
     -80, //3
     100, //4
   ]
   const images = collage.querySelectorAll('.collage-animation_image-wrapper')
 
-  const masterTl = gsap.timeline({
+  const openingTl = gsap.timeline({
     defaults: {
       ease: 'none',
+      //paused: true,
+    },
+  })
+  const closingTl = gsap.timeline({
+    defaults: {
+      ease: 'none',
+      //paused: true,
     },
   })
 
-  images.forEach((image, index) => {
+  const offsets = Array.from(images).map((image, index) => {
     let newIndex = index - images.length / 2 + 0.5
-    image.querySelector('.collage-animation_text').textContent = newIndex
-    // Calculate x and y offsets based on index
     let xOffset = offsetX[index]
     let yOffset = offsetY[index]
-
-    image.querySelector('.offset-number').textContent = offsetX[index]
-    masterTl.to(
-      image,
-      {
-        z: newIndex * 40,
-        x: xOffset,
-        y: yOffset,
-        scale: 1 + Math.abs(newIndex) * 0.1,
-        duration: 2,
-        ease: 'expo.inOut',
-      },
-      0
-    )
-
-    masterTl.to(
-      image,
-      {
-        z: newIndex,
-        x: 0,
-        y: 0,
-        scale: 1,
-        duration: 1,
-        ease: 'expo.inOut',
-      },
-      2.5
-    )
+    return { image, newIndex, xOffset, yOffset }
   })
+
+  openingTl.to(images, {
+    z: (index) => {
+      return offsets[index].newIndex * 40
+    },
+    x: (index) => {
+      return offsets[index].xOffset
+    },
+    y: (index) => {
+      return offsets[index].yOffset
+    },
+    duration: 4,
+    ease: hardExpo,
+  })
+
+  closingTl.to(images, {
+    z: (index) => {
+      return offsets[index].newIndex
+    },
+    x: 0,
+    y: 0,
+    scale: 1,
+    duration: 1.5,
+    ease: 'expo.inOut',
+  })
+
+  mainTl.add(openingTl, '0')
+  mainTl.add(closingTl, '-=3.8')
 }
