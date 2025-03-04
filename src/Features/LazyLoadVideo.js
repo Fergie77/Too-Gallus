9c1ff9b8348087ce394cdea27d401afa1ddba6ef
Fergie77 <48885644+Fergie77@users.noreply.github.com>
2025-03-04 16:28:17 +0000

    Update LazyLoadVideo.js

--- src/Features/LazyLoadVideo.js
+++ src/Features/LazyLoadVideo.js
@@ -4,13 +4,13 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 export const LazyLoadVideo = (container) => {
   gsap.registerPlugin(ScrollTrigger)
   const video = container.querySelectorAll('.lazy')
-  console.log(video)
+
   video.forEach((element) => {
     ScrollTrigger.create({
       trigger: element,
       start: 'top bottom',
       end: 'bottom top',
-      markers: true,
+
       onEnter: () => {
         element.play()
       },
