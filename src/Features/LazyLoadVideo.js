c33844bff918f29613720a2d423f79541a87c80e
Fergie77 <48885644+Fergie77@users.noreply.github.com>
2025-03-05 16:55:21 +0000

    u

--- src/Features/LazyLoadVideo.js
+++ src/Features/LazyLoadVideo.js
@@ -1,28 +1,51 @@
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+// import gsap from 'gsap'
+// import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 export const LazyLoadVideo = (container) => {
-  gsap.registerPlugin(ScrollTrigger)
-  const video = container.querySelectorAll('.lazy')
-  console.log(video)
-  video.forEach((element) => {
-    ScrollTrigger.create({
-      trigger: element,
-      start: 'top bottom',
-      end: 'bottom top',
-      markers: 'true',
-      onEnter: () => {
-        element.play()
+  // gsap.registerPlugin(ScrollTrigger)
+  // const video = container.querySelectorAll('.lazy')
+  // console.log(video)
+  // video.forEach((element) => {
+  //   ScrollTrigger.create({
+  //     trigger: element,
+  //     start: 'top bottom',
+  //     end: 'bottom top',
+  //     markers: 'true',
+  //     onEnter: () => {
+  //       element.play()
+  //     },
+  //     onEnterBack: () => {
+  //       element.play()
+  //     },
+  //     onLeave: () => {
+  //       element.pause()
+  //     },
+  //     onLeaveBack: () => {
+  //       element.pause()
+  //     },
+  //   })
+  // })
+
+  let videos = container.querySelectorAll('.lazy')
+
+  if (videos.length > 0) {
+    let observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            let video = entry.target
+            let source = video.getAttribute('data-src')
+            if (source) {
+              video.src = source
+              video.removeAttribute('data-src')
+            }
+            observer.unobserve(video)
+          }
+        })
       },
-      onEnterBack: () => {
-        element.play()
-      },
-      onLeave: () => {
-        element.pause()
-      },
-      onLeaveBack: () => {
-        element.pause()
-      },
-    })
-  })
+      { threshold: 0.1 }
+    )
+
+    videos.forEach((video) => observer.observe(video))
+  } else return
 }
