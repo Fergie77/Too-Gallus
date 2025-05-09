c00ddd1c1d8b2bb8d2b8d4bfd8bcb604d00c01df
Fergie77 <fergie2@live.co.uk>
2025-05-09 11:44:04 +0100

    Update getEvents.js

--- Netlify/Functions/getEvents.js
+++ Netlify/Functions/getEvents.js
@@ -1,12 +1,27 @@
-exports.handler = async function () {
-  const response = await fetch('https://ra.co/graphql', {
-    method: 'POST',
-    headers: {
-      'User-Agent': 'Mozilla/5.0',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      query: `query GET_POPULAR_EVENTS($filters: FilterInputDtoInput, $pageSize: Int) {
+exports.handler = async function (event) {
+  const eventId = event.queryStringParameters && event.queryStringParameters.id
+
+  let query, variables
+  if (eventId) {
+    // Query a specific event
+    query = `
+      query GET_EVENT($id: ID!) {
+        event(id: $id) {
+          id
+          title
+          attending
+          date
+          contentUrl
+          images { filename }
+          venue { name }
+        }
+      }
+    `
+    variables = { id: eventId }
+  } else {
+    // Query popular events (default)
+    query = `
+      query GET_POPULAR_EVENTS($filters: FilterInputDtoInput, $pageSize: Int) {
         eventListings(filters: $filters, pageSize: $pageSize, page: 1, sort: { attending: { priority: 1, order: DESCENDING } }) {
           data {
             event {
@@ -20,25 +35,36 @@ exports.handler = async function () {
             }
           }
         }
-      }`,
-      variables: {
-        filters: {
-          areas: { eq: 229 },
-          listingDate: { gte: '2023-06-01', lte: '2023-08-04' },
-          listingPosition: { eq: 1 },
-        },
-        pageSize: 10,
+      }
+    `
+    variables = {
+      filters: {
+        areas: { eq: 229 },
+        listingDate: { gte: '2023-06-01', lte: '2023-08-04' },
+        listingPosition: { eq: 1 },
       },
-    }),
+      pageSize: 10,
+    }
+  }
+
+  const response = await fetch('https://ra.co/graphql', {
+    method: 'POST',
+    headers: {
+      'User-Agent': 'Mozilla/5.0',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query, variables }),
   })
 
   const json = await response.json()
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*', // <-- This is the key line!
+      'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers': 'Content-Type',
     },
-    body: JSON.stringify(json.data.eventListings.data),
+    body: JSON.stringify(
+      eventId ? json.data.event : json.data.eventListings.data
+    ),
   }
 }
