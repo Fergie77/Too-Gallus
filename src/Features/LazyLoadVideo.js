6701e226c883eaca9d7a949d049fa844db66ddac
Fergie77 <fergie2@live.co.uk>
2025-07-30 14:57:06 +0100

    Update LazyLoadVideo.js

--- src/Features/LazyLoadVideo.js
+++ src/Features/LazyLoadVideo.js
@@ -2,30 +2,6 @@
 // import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 export const LazyLoadVideo = (container) => {
-  // gsap.registerPlugin(ScrollTrigger)
-  // const video = container.querySelectorAll('.lazy')
-  // console.log(video)
-  // video.forEach((element) => {
-  //   ScrollTrigger.create({
-  //     trigger: element,
-  //     start: 'top bottom',
-  //     end: 'bottom top',
-  //     markers: 'true',
-  //     onEnter: () => {
-  //       element.play()
-  //     },
-  //     onEnterBack: () => {
-  //       element.play()
-  //     },
-  //     onLeave: () => {
-  //       element.pause()
-  //     },
-  //     onLeaveBack: () => {
-  //       element.pause()
-  //     },
-  //   })
-  // })
-
   let videos = container.querySelectorAll('.lazy')
   if (videos.length > 0) {
     let observer = new IntersectionObserver(
